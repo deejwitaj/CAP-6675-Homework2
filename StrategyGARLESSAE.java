a469c1f09c16f07ecbca25346a54f56879abbed3
Didier <deejwitaj@knights.ucf.edu>
2018-11-18 18:49:50 -0500

    GARLESSAE Bug Fixes
    Fixed compile issues with GARLESSAE

--- StrategyGARLESSAE.java
+++ StrategyGARLESSAE.java
@@ -2,17 +2,14 @@
  * Class containing an type of Strategy.
  * @author	081028AW
  */
-public class StrategyGARLESSAE extends Object
+public class StrategyGARLESSAE extends Strategy
 {
   /**
    * Encoding for a strategy.
    */
 
-	int opponentLastMove = 1;
-	int myLastMove;
 	double cProb;
 	double dProb;
-	String name;
 	boolean swap = true;
 
 	// 0 = defect, 1 = cooperate
@@ -24,7 +21,6 @@ public class StrategyGARLESSAE extends Object
 		opponentLastMove = 1;
 		cProb = .5;
 		dProb = .5;
-		swap = true;
 	}  /* Strategy */
 
 	public int nextMove()
@@ -47,14 +43,6 @@ public class StrategyGARLESSAE extends Object
 			swap = true;
 			return 0;
 		}
-		return 0;
 	}  /* nextMove */
-
-
-	public void saveOpponentMove(int move)  { opponentLastMove = move; }
-	public int getOpponentLastMove()  { return opponentLastMove; }
-	public void saveMyMove(int move)  { myLastMove = move; }
-	public int getMyLastMove()  { return myLastMove; }
-	public String getName()  { return name; }
 }  /* class Strategy */
 
